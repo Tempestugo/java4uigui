ddc055044fc023509e8f30c4985ce6ab499f29e7
firstupdate <coffeture.blog@gmail.com>
2026-01-01 15:40:36 -0300

    Subindo arquivos

--- src/academy/devdojo/maratona/java/javacore/introducaoclasses/Bexercicios/dominio/test/TodosTest01.java
+++ src/academy/devdojo/maratona/java/javacore/introducaoclasses/Bexercicios/dominio/test/TodosTest01.java
@@ -9,16 +9,16 @@ import java.util.List;
 public class TodosTest01 {
     static void main(String[] args) {
         List<Produto> produtos2 = new ArrayList();
-        produtos2.add(new Produto("b", 20.0,2));
-        produtos2.add(new Produto("s",210,2));
-        produtos2.add(new Produto("s",210,2));
+        produtos2.add(new Produto("b", 20.0,2L));
+        produtos2.add(new Produto("s",210,2L));
+        produtos2.add(new Produto("s",210,2L));
 
         PipelineService<Produto, String> pipe = new PipelineService<>(produtos2);
         pipe.filtrar(produto -> produto.getPreco() < 210);
         pipe.consumidor(System.out::println);
         System.out.println("======================");
 
-        pipe.criadorDeObjeto(Produto::new);
+//        pipe.criadorDeObjeto(Produto::new);
 
         System.out.println("======================");
 
