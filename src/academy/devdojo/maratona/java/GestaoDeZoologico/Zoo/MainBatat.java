9d57836045c444b35728cb3c97a6ced3466b7316
firstupdate <coffeture.blog@gmail.com>
2026-01-03 00:27:57 -0300

    Subindo arquivos

---
+++ src/academy/devdojo/maratona/java/GestaoDeZoologico/Zoo/MainBatat.java
@@ -0,0 +1,26 @@
+package academy.devdojo.maratona.java.GestaoDeZoologico.Zoo;
+
+import academy.devdojo.maratona.java.GestaoDeZoologico.Animal;
+import academy.devdojo.maratona.java.GestaoDeZoologico.Jaula;
+import academy.devdojo.maratona.java.GestaoDeZoologico.Leao;
+import academy.devdojo.maratona.java.GestaoDeZoologico.Macaco;
+
+public class MainBatat {
+    static void main(String[] args) {
+        Animal animal = new Macaco("Pedro");
+        Animal animal2 = new Macaco("Hugo");
+
+        Animal animal3 = new Leao("J");
+        Animal animal4 = new Leao("Louis");
+
+        Jaula<Leao> jaulaDeLeao = new Jaula<>();
+        Animal macaco = new Macaco("Pedro");
+
+
+
+
+
+
+
+    }
+}
